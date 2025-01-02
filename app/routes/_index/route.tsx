17023c80d08c02ba7dd92e4c7712f65ad7425b5f
draxvint <polynar.david@gmail.com>
2025-01-02 09:58:34 +0100

    WIP third iteration over home, upload images to wix

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -15,7 +15,7 @@ export default function HomePage() {
         <>
             <div className="heroBanner">
                 <img
-                    src="https://s3-alpha-sig.figma.com/img/34eb/d38e/4737ca55766dd3c7e65a3f69a8937bc5?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=akfbVZ9I38apeW~rJ1XTtr7IhCMH-zOz0c8Vkuo00w9Z5rGoo2IJFxKnKJzfSIThBRE8KYkmMZGNHW9RjWtrjX7rwqeXxicUEBCQU73ygYZVhyMyVmjC8zTIBHyxi2OJKRwmXISZlfJC~22RH0oFSfnleot2QjT6MSGnBB4law8VwIi-9ebeHZ7afd9lSsdrne0CLlVfMXPNdGHEI~BPUGR1p3rbR9Lso4KHRGiad4Ysq-xycENG5zC~LgvQG4s8mrOWXAiEQSIRjObQo26G0h78cEBUsoW9ABmh86XFzKDwCo5LPQGIlMEN9AqRTObeEL1SM9qWCJRCIdT8krdFVA__"
+                    src="https://static.wixstatic.com/media/a2cc95_30c2b0d877354d389a2bce2e169440c9~mv2.png/v1/fit/w_640,h_640/4737ca55766dd3c7e65a3f69a8937bc5.png.png"
                     className="heroBannerImage"
                     alt=""
                 />
@@ -41,9 +41,7 @@ export default function HomePage() {
                     title={
                         "Fashion is more than just what you wear—it's how you express yourself. That’s why we’re committed to offering pieces that blend quality, comfort, and style, so you can look and feel your best every day."
                     }
-                    imageUrl={
-                        'https://s3-alpha-sig.figma.com/img/9dd3/fe0d/25846e5769d5dff3f39611ee1589857b?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=WsgAYeVcBWBuoo-zvP6AzvWq6etJft7oxhmwxZMsVoW8B7-RWKoAl51G-bCWAzjlqlloVk8iwkmmx-qddC5GsRG-r~i8lh4bU8VbaT1e~lQFuPhSI6gh0sAyDNNRzsJskAKKhaJtvVLwWb7rMUKSYKvNTOiiggOk5CXD4fy8-J2l64-RjQ6qZ-YltxF7TbPxGSLShQ8KEpBctHHjO27BCqMLS6aBk8f9GmEu7Y7ox9hyYpNGShe61yNMDJpTAkFT4h651XDN93WvuJ-Vn695yKcy1dRn8Wfc4SE7AZ0js0hCQr~6Csw5CqIpKwI2oXXCTuJH9Jj-ydzaDwfkIfsaJA__'
-                    }
+                    imageUrl="https://static.wixstatic.com/media/a2cc95_d312443aeecb499bb9f6986404b94193~mv2.png/v1/fit/w_640,h_640/25846e5769d5dff3f39611ee1589857b.png.png"
                 />
                 <FeaturedProductsSection
                     categorySlug="best-sellers"
@@ -54,64 +52,61 @@ export default function HomePage() {
                 <div className={styles.banner}>
                     <div className={styles.bannerContentWrapper}>
                         <div className={styles.bannerContent}>
-                            <span className={styles.bannerSubheading}>
-                                Product Spotlight
-                            </span>
+                            <span className={styles.bannerSubheading}>Product Spotlight</span>
                             <span className={styles.bannerTitle}>
                                 A hot summer deserves a cool hat
                             </span>
                         </div>
-                        <button onClick={() => navigate('/products/all-products')} className={classNames("button button-lg",styles.bannerButton)}>
-                          Shop now
+                        <button
+                            onClick={() => navigate('/products/all-products')}
+                            className={classNames('button button-lg', styles.bannerButton)}
+                        >
+                            Shop now
                         </button>
                     </div>
                     <div className={styles.bannerImageWrapper}>
-                        <img className={styles.bannerImage} src="https://s3-alpha-sig.figma.com/img/ae39/2e28/a9bfabda082c6167b007f5eda6ea0bf8?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NNEJH16XwLY1sAIUuudpWmNTRJ6gAvNXbo9nkUbS-Gh71eFm-6Ojrc-TsWEk-Jesl5Fc5SjsH1XIBtPzxuTqiBL-TiSPCq686vfjXlMkOdAf6EmSeKSnRDYz-LkW984Bt~NibG~8axA8tAW69IgieOVapjRv7~kyQl-73SDxeZqX70hFldrHsd4iJ0zYSIf1SRSqRJZ2mT4QsksZ1y~nk5a~W4BD59TAkRFq4HJXUTrMMv~AM1qdBzS4~wiBqkB5tJjVmKtbm3X4Gfxlgo18EEB8X9rWuzys5GCYtKz5Sc43akkUwPPa7ebHuLunKgQsDLlXq8Fg7LYar0dSXF9hbA__" alt="Product Spotlight" />
+                        <img
+                            className={styles.bannerImage}
+                            src="https://static.wixstatic.com/media/a2cc95_c3f3157d16424344a167c12f4e59af0d~mv2.png/v1/fit/w_640,h_640/a9bfabda082c6167b007f5eda6ea0bf8.png.png"
+                            alt="Product Spotlight"
+                        />
                     </div>
                 </div>
 
                 <div className={styles.infoSection}>
                     <div className={styles.infoSectionCard}>
-                        <span className={"material-symbols-outlined"}>local_shipping</span>
+                        <span className={'material-symbols-outlined'}>local_shipping</span>
                         <div className={styles.infoSectionCardTextWrapper}>
-                            <span className={styles.infoCardHeading}>
-                                Free Shipping
-                            </span>
+                            <span className={styles.infoCardHeading}>Free Shipping</span>
                             <span className={styles.infoSectionCardSubheading}>
                                 On orders over 120$
                             </span>
                         </div>
                     </div>
                     <div className={styles.infoSectionCard}>
-                        <span className={"material-symbols-outlined"}>refresh</span>
+                        <span className={'material-symbols-outlined'}>refresh</span>
                         <div className={styles.infoSectionCardTextWrapper}>
-                            <span className={styles.infoCardHeading}>
-Free Returns
-                            </span>
+                            <span className={styles.infoCardHeading}>Free Returns</span>
                             <span className={styles.infoSectionCardSubheading}>
-On full time priced items only
+                                On full time priced items only
                             </span>
                         </div>
                     </div>
                     <div className={styles.infoSectionCard}>
-                        <span className={"material-symbols-outlined"}>loyalty</span>
+                        <span className={'material-symbols-outlined'}>loyalty</span>
                         <div className={styles.infoSectionCardTextWrapper}>
-                            <span className={styles.infoCardHeading}>
-Crash replacement
-                            </span>
+                            <span className={styles.infoCardHeading}>Crash replacement</span>
                             <span className={styles.infoSectionCardSubheading}>
-40% off your new kit
+                                40% off your new kit
                             </span>
                         </div>
                     </div>
                     <div className={styles.infoSectionCard}>
-                        <span className={"material-symbols-outlined"}>recycling</span>
+                        <span className={'material-symbols-outlined'}>recycling</span>
                         <div className={styles.infoSectionCardTextWrapper}>
-                            <span className={styles.infoCardHeading}>
-Eco-friendly
-                            </span>
+                            <span className={styles.infoCardHeading}>Eco-friendly</span>
                             <span className={styles.infoSectionCardSubheading}>
-All of our packacing is recycled
+                                All of our packacing is recycled
                             </span>
                         </div>
                     </div>
--- src/components/tagged-products-section/tagged-products-section.tsx
+++ src/components/tagged-products-section/tagged-products-section.tsx
@@ -1,16 +1,53 @@
-import styles from './tagged-products-section.module.scss'
+import styles from './tagged-products-section.module.scss';
 import { ProductsSpotlight } from '~/src/components/products-spotlight/products-spotlight';
 export const TaggedProductsSection = () => {
-  return (
-    <div className={styles.root}>
-      <div className={styles.header}>
-        <div className={"subheading"}>Sale is on</div>
-        <h3 className={"uppercase"}>Don’t miss our last catches</h3>
-      </div>
-      <div className={styles.content}>
-        <ProductsSpotlight spotlights={[{ x: 0.57, y: 0.28,   productId: '123',   productName: 'Earrings',   price: '$9.00' },{x: 0.4, y: 0.6, productId: '123', productName: 'Woman’s T-shirt', price: '$14.00'}]} imagePosition={"top"} imageUrl={"https://s3-alpha-sig.figma.com/img/97ab/b5f7/0e228a0f121297eada19e8519cd7c75e?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ae0goRyAtOj6jv4CIU2bjZQw0A9aOB09c1MqLyUec~ew8xt4aPp-Kmmd--Qv7DmX~raRMtFW~QC6gZgZ1gDxYDaZtevMZdHv3XDUWQI-VTIw9ka~FIZXyTaZQz-4Rpyn7a5XdnEnhSWq5C2HzYdHY1Xm6YGeeKzmfQNIav80mP0SYSg9jrbciwKaShu6BZODNrgfJ0Fpo~8SdRvY~HjJjVJLZ09ON97jliapOgMqApclMsDake2Uazs2xzJ7Ttnv-65R6snVzw0AhZowDUc1eaz0nrLJF4DMURgQxOCVu85SuidVrVOLPecWnO1tJQYzyUKI~ewy3RJYyauXuQwvbA__"}/>
-        <ProductsSpotlight spotlights={[{ x: 0.57, y: 0.20,   productId: '123',   productName: 'Beanie',   price: '$11.00' },{x: 0.4, y: 0.8, productId: '123', productName: 'Man’s T-shirt', price: '$14.00'}]} imageUrl={"https://s3-alpha-sig.figma.com/img/66a0/3109/9a9999cd3f47e2952e55fc45ae9f75b5?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=buAoklPQfelMqL8o5Cpyn3gp8yIum5R75YPaEG2FUy5nHfhJUlr~g61jxCTuGPnTViHfHgpvu19PVwQAYHpdkydPfd9cOIZNWXl11PwfSePGnXXHoD-gC9hK~Cnsd4DSgOdJWuDDyKbRyO0sZzQzX~7UTduAPiiCzOIMJdaqRXJOXspd8z3X8kGe1snvyAl0fNbtqrh1z~2hYy1YW~8ytoaOS4Aa4U0T2RZsHMGA7f1-x1yiWZNUr6ALj0uK3rNXe8UJ5cUJobolozWQZ5Prp55T4jM4-pzTYK-AACZm-u3yC4XayA74Gm5~sIULuVrT4f3KxPEC03ZbWkH9Q3HDtQ__"}/>
-      </div>
-    </div>
-  )
-}
+    return (
+        <div className={styles.root}>
+            <div className={styles.header}>
+                <div className={'subheading'}>Sale is on</div>
+                <h3 className={'uppercase'}>Don’t miss our last catches</h3>
+            </div>
+            <div className={styles.content}>
+                <ProductsSpotlight
+                    spotlights={[
+                        {
+                            x: 0.57,
+                            y: 0.28,
+                            productId: '123',
+                            productName: 'Earrings',
+                            price: '$9.00',
+                        },
+                        {
+                            x: 0.4,
+                            y: 0.6,
+                            productId: '123',
+                            productName: 'Woman’s T-shirt',
+                            price: '$14.00',
+                        },
+                    ]}
+                    imagePosition={'top'}
+                    imageUrl="https://static.wixstatic.com/media/a2cc95_11cce258e7cb45ab80637d887a5e8aea~mv2.png/v1/fit/w_640,h_640/0e228a0f121297eada19e8519cd7c75e.png.png"
+                />
+                <ProductsSpotlight
+                    spotlights={[
+                        {
+                            x: 0.57,
+                            y: 0.2,
+                            productId: '123',
+                            productName: 'Beanie',
+                            price: '$11.00',
+                        },
+                        {
+                            x: 0.4,
+                            y: 0.8,
+                            productId: '123',
+                            productName: 'Man’s T-shirt',
+                            price: '$14.00',
+                        },
+                    ]}
+                    imageUrl="https://static.wixstatic.com/media/a2cc95_547fc6927ad4401e92ada183ffcfffcf~mv2.png/v1/fit/w_640,h_640/9a9999cd3f47e2952e55fc45ae9f75b5.png.png"
+                />
+            </div>
+        </div>
+    );
+};
