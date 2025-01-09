173afc5a79db52604596460f869e437c9d706b84
draxvint <polynar.david@gmail.com>
2025-01-09 17:03:38 +0100

    my orders page

--- app/routes/members-area.my-orders/route.tsx
+++ app/routes/members-area.my-orders/route.tsx
@@ -4,13 +4,12 @@ import {
     type LoaderFunctionArgs,
     type MetaFunction,
 } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { useLoaderData, useNavigate } from '@remix-run/react';
 import { type OrderDetails } from '~/src/wix/ecom';
 import { initializeEcomApiForRequest } from '~/src/wix/ecom/session';
-import { OrderSummary } from '~/src/components/order-summary/order-summary';
-import { Accordion } from '~/src/components/accordion/accordion';
 import { CategoryLink } from '~/src/components/category-link/category-link';
 import { loaderMockData } from './loader-mock-data';
+
 
 import styles from './route.module.scss';
 
@@ -34,6 +33,7 @@ export async function coduxLoader(): ReturnType<typeof loader> {
 }
 
 export default function MyOrdersPage() {
+    const navigate = useNavigate()
     const { orders } = useLoaderData<typeof loader>();
 
     const formatOrderCreationDate = (date: Date) =>
@@ -43,48 +43,60 @@ export default function MyOrdersPage() {
             year: 'numeric',
         });
 
-    const accordionItems = orders.map((order) => ({
-        header: (
-            <div className={styles.orderHeader}>
-                <div className={styles.orderHeaderSection}>
-                    <span className={styles.orderHeaderSectionName}>Date: </span>
-                    {formatOrderCreationDate(new Date(order._createdDate!))}
-                </div>
-                <div className={styles.orderHeaderSection}>
-                    <span className={styles.orderHeaderSectionName}>Order: </span>
-                    {order.number}
-                </div>
-                <div className={styles.orderHeaderSection}>
-                    <span className={styles.orderHeaderSectionName}>Status: </span>
-                    {order.status}
-                </div>
-                <div className={styles.orderHeaderSection}>
-                    <span className={styles.orderHeaderSectionName}>Total: </span>
-                    {order.priceSummary?.total?.formattedAmount}
-                </div>
-            </div>
-        ),
-        content: <OrderSummary key={order._id} order={order} />,
-    }));
-
     return (
         <div>
-            <div className={styles.pageHeader}>
-                <div className={styles.title}>My Orders</div>
-                <div className={styles.message}>
-                    View your order history or check the status of a recent order.
-                </div>
-            </div>
             <div className={styles.orders}>
                 {orders.length > 0 ? (
                     <>
                         <div className={styles.orderListHeader}>
-                            <div className={styles.orderListHeaderSection}>Date</div>
                             <div className={styles.orderListHeaderSection}>Order</div>
+                            <div className={styles.orderListHeaderSection}>Date</div>
                             <div className={styles.orderListHeaderSection}>Status</div>
                             <div className={styles.orderListHeaderSection}>Total</div>
+                            <div className={styles.orderListHeaderSection}></div>
                         </div>
-                        <Accordion items={accordionItems} className={styles.orderList} />
+
+                        {orders.map((order) => (
+                            <div key={order._id} className={styles.orderHeaderWrapper}>
+                                <div className={styles.orderHeader}>
+                                    <div className={styles.orderHeaderSection}>
+                                        <span className={styles.orderHeaderSectionName}>
+                                            Order:{' '}
+                                        </span>
+                                        {order.number}
+                                    </div>
+                                    <div className={styles.orderHeaderSection}>
+                                        <span className={styles.orderHeaderSectionName}>
+                                            Date:{' '}
+                                        </span>
+                                        {formatOrderCreationDate(new Date(order._createdDate!))}
+                                    </div>
+
+                                    <div className={styles.orderHeaderSection}>
+                                        <span className={styles.orderHeaderSectionName}>
+                                            Status:{' '}
+                                        </span>
+                                        {order.status}
+                                    </div>
+                                    <div className={styles.orderHeaderSection}>
+                                        <span className={styles.orderHeaderSectionName}>
+                                            Total:{' '}
+                                        </span>
+                                        {order.priceSummary?.total?.formattedAmount}
+                                    </div>
+                                </div>
+                                <div>
+                                    <button
+                                        onClick={() =>
+                                            navigate(`/members-area/my-orders/${order._id}`)
+                                        }
+                                        className="button"
+                                    >
+                                        View Order
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
                     </>
                 ) : (
                     <div className={styles.emptyStateContainer}>
--- app/routes/members-area/route.tsx
+++ app/routes/members-area/route.tsx
@@ -1,3 +1,4 @@
+import '../../../src/styles/utils.scss';
 import classNames from 'classnames';
 import { Outlet, NavLink } from '@remix-run/react';
 
@@ -5,18 +6,21 @@ import styles from './route.module.scss';
 
 export default function MembersAreaPage() {
     const tabClassName = ({ isActive }: { isActive: boolean }) => {
-        return classNames('tab', { active: isActive });
+        return classNames('tab action', { active: isActive });
     };
 
     return (
         <div className={styles.page}>
-            <div className="tabs">
-                <NavLink to={'/members-area/my-account'} className={tabClassName}>
-                    My Account
-                </NavLink>
-                <NavLink to={'/members-area/my-orders'} className={tabClassName}>
-                    My Orders
-                </NavLink>
+            <div className={styles.header}>
+                <h1 className="heading1 uppercase">Profile</h1>
+                <div className="tabs">
+                    <NavLink to={'/members-area/my-orders'} className={tabClassName}>
+                        My Orders
+                    </NavLink>
+                    <NavLink to={'/members-area/my-account'} className={tabClassName}>
+                        My Account
+                    </NavLink>
+                </div>
             </div>
             <div className={styles.content}>
                 <Outlet />
